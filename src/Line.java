922a42d4f932d0470b65cfb866b4bbaf3807e63b
Artem Melnikov <a.melnikov@carex.su>
2016-11-25 16:05:01 +0600

    Готовая

--- src/Line.java
+++ src/Line.java
@@ -3,17 +3,16 @@ public class Line {
     public int X2;
     public int Y1;
     public int Y2;
-    public int k;
-    public int b;
-    public String urav;
+    public int A, B, C;
 
-    Line(int x1, int x2, int y1, int y2){
+    Line(int x1, int y1, int x2, int y2){
         X1 = x1;
         X2 = x2;
         Y1 = y1;
         Y2 = y2;
-        k = (Y1 - Y2)/(X1-X2);
-        b = Y2 - (k*X2);
+        A = (Y1 - Y2);
+        B = (X2 - X1);
+        C = (X1*Y2 - X2*Y1);
     }
 
     public int getX1() {
@@ -33,17 +32,18 @@ public class Line {
     }
 
     public String getUrav() {
-        if (b>0) urav = "y="+k+"x"+"+"+b;
-        if (b<0) urav = "y="+k+"x"+b;
-        if (b==0) urav = "y="+k+"x";
-        return urav;
+        return A+"x"+B+"y"+C+"=0";
     }
 
-    public int getK() {
-        return k;
+    public int getA() {
+        return A;
     }
 
     public int getB() {
-        return b;
+        return B;
+    }
+
+    public int getC() {
+        return C;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,26 +4,26 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Line line1 = new Line(1,2,3,4);
-        Line line2 = new Line(45,3,2,1);
+        Line line1 = new Line(0,1,1,1);
+        Line line2 = new Line(0,0,1,0);
+        int A1 = line1.getA();
+        int A2 = line2.getA();
+        int B1 = line1.getB();
+        int B2 = line2.getB();
+        int temps;
 
         System.out.println("Уравнение прямой 1:" + line1.getUrav());
         System.out.println("Уравнение прямой 2:" + line2.getUrav());
 
-        if (line1.getK() == line2.getK()){
+        temps = (A1*B2)-(A2*B1);
+        if (temps == 0){
             System.out.println("Прямые параллельны");
         }
         else{
-            int A1 = line1.getK();
-            int A2 = line2.getK();
-            int TG = (A2-A1)/(1+(A1*A2));
-            if (TG == 0){
-                System.out.println("Угол равен 90 градусов");
-            }
-            else{
-                double rad = Math.atan(TG);
-                System.out.println("Угол:"+ (rad*180/Math.PI));
-            }
+            double ugol;
+            ugol = Math.acos((((B1*B2)+(A1*A2))/((Math.sqrt(Math.pow(B1,2)+Math.pow(A1,2)))*(Math.sqrt((Math.pow(B2,2)+(Math.pow(A2,2))))))));
+            ugol = (ugol * 180) / Math.PI;
+            System.out.println("Угол между прямыми: " + ugol);
         }
 }
 }
